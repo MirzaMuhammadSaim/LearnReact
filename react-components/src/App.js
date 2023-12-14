613217e7786269acf1d2d443bac713ee35bcf097
[MirzaMuhammadSaim] <[saimnasir820@gmail.com]>
2023-12-14 11:23:32 -0800

    LearnReact

---
+++ react-components/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Newuser from './Components/Newuser';
+import Home from './Components/Home';
+import About from './Components/About';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <Newuser/>
+      <Home/>
+      <About/>
+
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ react-components/src/Components/Newuser.jsx
@@ -0,0 +1,8 @@
+function Newuser(){
+    return(
+        <div>
+            <h1>hello new user components</h1>
+        </div>
+    );
+}
+export default Newuser;
